467c5097e9da44ed939a3d8003f26a67ae868e2b
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-08-30 18:32:46 +0000

    Now using forEach

--- callbackIntro.js
+++ callbackIntro.js
@@ -1,10 +1,10 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
-        found(i);   // execute callback
-      }
-    }
+    arr.forEach((element, index) => {
+        if (element === "Waldo") {
+            found(index);
+        }
+    });
   }
   
   function actionWhenFound(index) {
